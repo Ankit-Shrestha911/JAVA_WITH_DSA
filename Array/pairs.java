a55cc5de11ef1972f3542022fa559d931c80275a
Ankit-Shrestha911 <ankitshrestha911@gmail.com>
2023-05-02 23:10:09 +0530

    11:09

---
+++ Array/pairs.java
@@ -0,0 +1,22 @@
+public class pairs{
+    
+    public static void printPair(int arr[])
+    {
+        for(int i=1; i<arr.length-1; i++)
+        {
+            for(int j=i+1; j<arr.length; j++)
+            {
+                System.out.print("("+arr[i]+","+arr[j]+") ");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
+    public static void main(String[] args){
+        int arr[] = {2,4,6,8,10};
+        printPair(arr);
+    }
+}
